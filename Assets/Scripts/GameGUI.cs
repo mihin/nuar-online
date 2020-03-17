cfac8d52541d0d1b0d74095a355c8161d1992d02
mikhail.prisheltsev <mikhail.prisheltsev@gmail.com>
2020-03-17 17:59:35 +0300

    Added game ui dummy buttons

---
+++ Assets/Scripts/GameGUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameGUI : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button ShootButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        //ShootButton.onClick += onShootClick;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+        
+
+}
